a318a9cd8d22a1ce7111c9888169e2642298f0eb
杰克有茶 <chenwansal1@163.com>
2022-11-09 19:33:44 +0800

    <feature> add: Get Candidate

--- Assets/com.frame.fpmath/Sample/Sample_QuadTree.cs
+++ Assets/com.frame.fpmath/Sample/Sample_QuadTree.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using FixMath.NET;
 
@@ -27,24 +28,29 @@ namespace JackFrame.FPMath.Sample {
         int sizeMax = 100;
         int sizeMin = 1;
 
+        FPVector2 mousePos;
+        FPVector2 mouseSize = new FPVector2(20, 20);
+
+        List<FPQuadTreeNode<string>> candidates;
+
         void Awake() {
             Console.SetOut(new UnityTextWriter());
             rd = new System.Random();
             tree = new FPQuadTree<string>(width, height, 12);
-            System.Console.WriteLine("yo");
+            candidates = new List<FPQuadTreeNode<string>>();
         }
 
         void Update() {
 
+            var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos = new FPVector2(FP64.ToFP64(worldPos.x), FP64.ToFP64(worldPos.y));
             if (Input.GetMouseButtonDown(1)) {
-                var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                tree.Insert("1", new FPBounds2(
-                    new FPVector2(FP64.ToFP64(worldPos.x), FP64.ToFP64(worldPos.y)),
-                    new FPVector2(rd.Next(sizeMin, sizeMax), rd.Next(sizeMin, sizeMax))
+                tree.Insert("1", new FPBounds2(mousePos, new FPVector2(rd.Next(sizeMin, sizeMax), rd.Next(sizeMin, sizeMax))
                 ));
             }
 
-            // System.Console.WriteLine(worldPos.ToString());
+            candidates.Clear();
+            tree.GetCandidates(new FPBounds2(mousePos, mouseSize), candidates);
 
         }
 
@@ -55,6 +61,20 @@ namespace JackFrame.FPMath.Sample {
 
             Gizmos.color = Color.red;
             tree.Traval(value => {
+                var center = value.Bounds.center;
+                var size = value.Bounds.size;
+                Gizmos.DrawWireCube(new Vector3(center.x.AsFloat(), center.y.AsFloat()), new Vector3(size.x.AsFloat(), size.y.AsFloat()));
+            });
+
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireCube(new Vector3(mousePos.x.AsFloat(), mousePos.y.AsFloat()), new Vector3(mouseSize.x.AsFloat(), mouseSize.y.AsFloat()));
+
+            if (candidates == null) {
+                return;
+            }
+
+            Gizmos.color = Color.green;
+            candidates.ForEach(value => {
                 var center = value.Bounds.center;
                 var size = value.Bounds.size;
                 Gizmos.DrawWireCube(new Vector3(center.x.AsFloat(), center.y.AsFloat()), new Vector3(size.x.AsFloat(), size.y.AsFloat()));
