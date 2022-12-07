03ce823fd70d3675e9ccb1f1179715f8e1491e4e
杰克有茶 <chenwansal1@163.com>
2022-12-07 14:37:06 +0800

    <fix> 宏定义 CHECKMATH 时出错

--- Assets/com.frame.fpmath/PurePlugins/FixedMath.Net/src/FP64/FP64.cs
+++ Assets/com.frame.fpmath/PurePlugins/FixedMath.Net/src/FP64/FP64.cs
@@ -278,8 +278,8 @@ namespace FixMath.NET {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static FP64 operator +(FP64 x, FP64 y) {
 #if CHECKMATH
-			var xl = x.m_rawValue;
-            var yl = y.m_rawValue;
+			var xl = x.RawValue;
+            var yl = y.RawValue;
             var sum = xl + yl;
             // if signs of operands are equal and signs of sum and x are different
             if (((~(xl ^ yl) & (xl ^ sum)) & MIN_VALUE) != 0) {
@@ -309,8 +309,8 @@ namespace FixMath.NET {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static FP64 operator -(FP64 x, FP64 y) {
 #if CHECKMATH
-			var xl = x.m_rawValue;
-            var yl = y.m_rawValue;
+			var xl = x.RawValue;
+            var yl = y.RawValue;
             var diff = xl - yl;
             // if signs of operands are different and signs of sum and x are different
             if ((((xl ^ yl) & (xl ^ diff)) & MIN_VALUE) != 0) {
@@ -346,8 +346,8 @@ namespace FixMath.NET {
                 return Zero;
             }
 #if CHECKMATH
-			var xl = x.m_rawValue;
-            var yl = y.m_rawValue;
+			var xl = x.RawValue;
+            var yl = y.RawValue;
 
             var xlo = (ulong)(xl & 0x00000000FFFFFFFF);
             var xhi = xl >> FRACTIONAL_PLACES;
